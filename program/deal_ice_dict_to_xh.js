d9ba65acc457efd5acfc4d4ced7b213a2f9e7819
gaboolic <23441099@qq.com>
2024-02-28 16:15:58 +0800

    雾凇词库加辅助码

--- program/deal_ice_dict_to_xh.js
+++ program/deal_ice_dict_to_xh.js
@@ -31,7 +31,7 @@ dictLines.forEach((line) => {
 console.log("孙 " + dictData['孙'])
 
 // 需要修改的YAML文件的路径
-const yamlFilePath = path.join(__dirname, '../cn_dicts/base.dict.yaml');
+const yamlFilePath = path.join(__dirname, '../cn_dicts/8105.dict.yaml');
 
 // 同步读取YAML文件
 const yamlFileContent = fs.readFileSync(yamlFilePath, 'utf8');
@@ -64,112 +64,126 @@ function updateMissingEncodings(filePath) {
     // Process each line
     lines.forEach((line) => {
         // Assume each line has the format "字\t编码"
-        if (line.includes('\t') && !line.startsWith("#")) {
-            const [character, encoding, frequency] = line.split('\t');
-
-            // console.log(encoding)
-
-            pinyin_list = encoding.split(" ")
-            double_list = ""
-            var pinyin_index = 0
-            for (pinyin of pinyin_list) {
-                double_pinyin = ""
-                if (pinyin.length == 2) {
-                    double_pinyin = pinyin
-                } else if (pinyin.length == 1) {
-                    double_pinyin = pinyin + pinyin
-                } else {
-                    shengmu = pinyin.substring(0, 1)
-                    yunmu = pinyin.substring(1)
-
-                    double_shengmu = shengmu
-                    if (pinyin.charAt(1) == "h") {
-                        shengmu = pinyin.substring(0, 2)
-                        yunmu = pinyin.substring(2)
-                        shengmu_map = { "zh": "v", "ch": "i", "sh": "u" }
-                        double_shengmu = shengmu_map[shengmu]
-                    }
-                    if (double_shengmu == 'a') {
-                        yunmu = pinyin
-                    }
-                    if (double_shengmu == 'o') {
-                        yunmu = pinyin
-                    }
-                    const yunmu_map = {
-                        'iu': 'q',
-                        'ei': 'w',
-                        'uan': 'r',
-                        'van': 'r',
-
-                        'ue': 't',
-                        've': 't',
-                        'un': 'y',
-                        'vn': 'y',
-                        'uo': 'o',
-                        'ie': 'p',
-
-                        'iong': 's',
-                        'ong': 's',
-
-                        'ai': 'd',
-                        'en': 'f',
-
-                        'eng': 'g',
-                        'ang': 'h',
-                        'an': 'j',
-
-                        'ing': 'k',
-                        'uai': 'k',
-                        'iang': 'l',
-                        'uang': 'l',
-
-                        'ou': 'z',
-                        'ia': 'x',
-                        'ua': 'x',
-                        'ao': 'c',
-                        'ui': 'v',
-                        'in': 'b',
-
-                        'iao': 'n',
-
-                        'ian': 'm',
-                    };
-                    double_yummu = yunmu_map[yunmu]
-                    if (yunmu.length == 1)
-                        double_yummu = yunmu
-
-                    double_pinyin = double_shengmu + double_yummu
-
-                    if (double_pinyin.length != 2) {
-                        console.log("!!!!double_pinyin " + double_pinyin + " " + pinyin + " ")
-                    }
-                }
-                var clean_character = character.replace("·", "")
-                var character_encoding_pre = clean_character.charAt(pinyin_index)
-                encoding_post = dictData[character_encoding_pre];
-                if (encoding_post == null) {
-                    // console.log("!!!character_encoding_pre:" + character_encoding_pre)
-                    // console.log("!!!!encoding_post==null " + double_pinyin + " " + pinyin + " character_encoding_pre:" + character_encoding_pre + " " + line)
-                    // console.log("character:" + character)
-                    // console.log("pinyin_index " + pinyin_index)
-                    // console.log("pinyin " + pinyin)
-                    // console.log("character.charAt(pinyin_index):" + character.charAt(pinyin_index))
-                    // console.log("double_pinyin " + double_pinyin)
-                }
-                double_list += double_pinyin + "[" + encoding_post
-                double_list += " "
-                pinyin_index++
-            }
-            double_list = double_list.substring(0, double_list.length - 1)
-            // Update the line with the missing encoding
-            const updatedLine = `${character}\t${double_list}\t${frequency}`;
-            console.log(updatedLine)
-            // Append the updated line to the updated content
-            updatedContent += updatedLine + '\n';
-        } else {
+        if (!line.includes('\t') || line.startsWith("#")) {
             // Append non-matching lines to the updated content
             updatedContent += line + '\n';
+            return
         }
+
+        const [character, encoding, frequency] = line.split('\t');
+
+
+        if (encoding == "100") {
+            // Append non-matching lines to the updated content
+            updatedContent += line + '\n';
+            return
+        }
+        //console.log(encoding)
+        pinyin_list = encoding.split(" ")
+        double_list = ""
+        var pinyin_index = 0
+        for (pinyin of pinyin_list) {
+            double_pinyin = ""
+            if (pinyin.length == 2) {
+                double_pinyin = pinyin
+            } else if (pinyin.length == 1) {
+                double_pinyin = pinyin + pinyin
+            } else {
+                shengmu = pinyin.substring(0, 1)
+                yunmu = pinyin.substring(1)
+
+                double_shengmu = shengmu
+                if (pinyin.charAt(1) == "h") {
+                    shengmu = pinyin.substring(0, 2)
+                    yunmu = pinyin.substring(2)
+                    shengmu_map = { "zh": "v", "ch": "i", "sh": "u" }
+                    double_shengmu = shengmu_map[shengmu]
+                }
+                if (double_shengmu == 'a') {
+                    yunmu = pinyin
+                }
+                if (double_shengmu == 'o') {
+                    yunmu = pinyin
+                }
+                if (double_shengmu == 'e') {
+                    yunmu = pinyin
+                }
+                const yunmu_map = {
+                    'iu': 'q',
+                    'ei': 'w',
+                    'uan': 'r',
+                    'van': 'r',
+
+                    'ue': 't',
+                    've': 't',
+                    'un': 'y',
+                    'vn': 'y',
+                    'uo': 'o',
+                    'ie': 'p',
+
+                    'iong': 's',
+                    'ong': 's',
+
+                    'ai': 'd',
+                    'en': 'f',
+
+                    'eng': 'g',
+                    'ang': 'h',
+                    'an': 'j',
+
+                    'ing': 'k',
+                    'uai': 'k',
+                    'iang': 'l',
+                    'uang': 'l',
+
+                    'ou': 'z',
+                    'ia': 'x',
+                    'ua': 'x',
+                    'ao': 'c',
+                    'ui': 'v',
+                    'in': 'b',
+
+                    'iao': 'n',
+
+                    'ian': 'm',
+                };
+                double_yummu = yunmu_map[yunmu]
+                if (yunmu.length == 1)
+                    double_yummu = yunmu
+
+                double_pinyin = double_shengmu + double_yummu
+
+                if (double_pinyin.length != 2) {
+                    console.log("!!!!double_pinyin " + double_pinyin + " " + pinyin + " ")
+                }
+            }
+            var clean_character = character.replace("·", "")
+            var character_encoding_pre = clean_character.charAt(pinyin_index)
+            encoding_post = dictData[character_encoding_pre];
+            if (encoding_post == null) {
+                // console.log("!!!character_encoding_pre:" + character_encoding_pre)
+                // console.log("!!!!encoding_post==null " + double_pinyin + " " + pinyin + " character_encoding_pre:" + character_encoding_pre + " " + line)
+                // console.log("character:" + character)
+                // console.log("pinyin_index " + pinyin_index)
+                // console.log("pinyin " + pinyin)
+                // console.log("character.charAt(pinyin_index):" + character.charAt(pinyin_index))
+                // console.log("double_pinyin " + double_pinyin)
+                encoding_post = "["
+            }
+            double_list += double_pinyin + "[" + encoding_post
+            double_list += " "
+            pinyin_index++
+        }
+        double_list = double_list.substring(0, double_list.length - 1)
+        // Update the line with the missing encoding
+        var updatedLine = `${character}\t${double_list}\t${frequency}`;
+        if (frequency == null) {
+            updatedLine = `${character}\t${double_list}`;
+        }
+
+        // Append the updated line to the updated content
+        updatedContent += updatedLine + '\n';
+
     });
 
     // Write the updated content back to the file
