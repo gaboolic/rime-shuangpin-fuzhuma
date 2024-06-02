3e58bfa04b2da58105c7c6665645097e7557b1bc
gaboolic <23441099@qq.com>
2024-06-02 21:05:07 +0800

    英汉互译

--- program/opencc/deal_e_chinese.py
+++ program/opencc/deal_e_chinese.py
@@ -1,5 +1,6 @@
 write_file = open('cn_dicts_dazhu/english_chinese.txt', 'w')
 
+word_list = []
 with open('opencc/english_chinese.txt', 'r', encoding='utf-8') as dict_file:
     # 一下	一下 once / all of a sudden
     for line in dict_file:
@@ -10,11 +11,17 @@ with open('opencc/english_chinese.txt', 'r', encoding='utf-8') as dict_file:
 
         english = params[0]
         chinese = params[1]
-        chinese = chinese.replace(" "," ")
+        chinese = chinese.replace(" ","")
         chinese = chinese.replace("@"," ")
 
         for word in english.split("|"):
-
+            if word == '':
+                continue
+            if word in word_list:
+                print("!!!!!"+word)
+                print(line)
+            else :
+                word_list.append(word)
             new_line = word + "\t" + word +" "
             new_line += chinese
             write_file.write(new_line+"\n")
