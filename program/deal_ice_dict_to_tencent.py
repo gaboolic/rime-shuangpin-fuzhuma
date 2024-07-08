d2301e78ffe9582ff7a8b3b90a1b0370611ceb20
gaboolic <23441099@qq.com>
2024-07-08 14:40:32 +0800

    更新词频

--- program/deal_ice_dict_to_tencent.py
+++ program/deal_ice_dict_to_tencent.py
@@ -36,8 +36,8 @@ def update_missing_encodings(file_path, write_file_path, dict_data):
         
         
         if "tencent" in file_path :
-            updated_line = f"{character}\t30"
-            updated_content += updated_line + '\n'
+            # updated_line = f"{character}\t30"
+            updated_content += line + '\n'
             continue
         else:
             if encoding == "100":
@@ -134,13 +134,13 @@ def update_missing_encodings(file_path, write_file_path, dict_data):
             # updated_content += line + '\n'
             pass
 
-        if "tencent" in file_path :
+        if "tencent" in file_path and False:
             updated_line = f"{character}\t99"
         else:
             if frequency is not None:
                 updated_line = f"{character}\t{double_list}\t{frequency}"
             else :
-                updated_line = f"{character}\t{double_list}"
+                updated_line = f"{character}\t{double_list}\t1"
         updated_content += updated_line + '\n'
 
     # Write the updated content back to the file
